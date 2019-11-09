01fe919059ca02b3bb293c22a54e67d18589938a
BakerFan <1037114479@qq.com>
2019-11-09 22:57:53 +0800

    initialized version

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    private Camera cam;
+    private float timeHit = 0f;         //用于点击的时间间隔,每次点击时间间隔应大于0.2秒  
+    private GameObject vegetable_basket;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        vegetable_basket = GameObject.FindGameObjectWithTag("vege_bag");
+        //Debug.Log(vegetable_basket);
+    }
+
+    void Update()
+    {
+        timeHit += Time.deltaTime;
+        if (timeHit > 0.2f)
+        {
+            //Input.simulateMouseWithTouches = true;
+            if (Input.GetMouseButton(0))
+            {
+                timeHit = 0f;
+                RaycastHit hit;
+                bool isHit = Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, 100f);
+                //Debug.Log(isHit);
+                Debug.Log(hit.collider.gameObject);
+                if (isHit&&hit.collider.gameObject==vegetable_basket)
+                {
+                    //Debug.Log("in!");
+                    vegetable_basket.SendMessage("OnTouched", SendMessageOptions.DontRequireReceiver);
+                }
+                
+            }
+        }
+    }  
+}
---
+++ Assets/Scripts/VegetableBasket.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegetableBasket : MonoBehaviour
+{
+    private GameObject bag;
+    private GameObject[] bag_items=new GameObject [9];
+    private int count=0;
+
+    // Start is called before the first frame 
+    void Start()
+    {
+        bag= GameObject.FindGameObjectWithTag("bag");
+        bag.SetActive(false);
+        for(; count<bag.transform.childCount-1;count++)
+        {
+            bag_items[count] = bag.transform.GetChild(count+1).gameObject;
+            Debug.Log(bag_items[count]);
+        }
+    }
+
+    void OnTouched()
+    {
+        bag.SetActive(true);
+    }
+}
